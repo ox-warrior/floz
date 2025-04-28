97e16173b26fde4860f513f3830de97d0e8750bd
ox-warrior <898287771@qq.com>
2025-04-28 16:05:02 +0800

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # floz
+
+## Apache
--- main.go
+++ main.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go install github.com/google/addlicense@latest
+
 package main
 
 import "fmt"
